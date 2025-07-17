ac299ebe6270aaa21a126fd877305320add7fc22
reubenmiller <reuben.d.miller@gmail.com>
2025-07-17 08:05:13 +0200

    disable rust format for generated code due to issue running under yocto

--- crates/tools/rugix-ctrl/build.rs
+++ crates/tools/rugix-ctrl/build.rs
@@ -1,6 +1,7 @@
 fn main() {
     sidex_build_rs::configure()
         .with_bundle(".")
+        .format_generated_code(false)
         .generate()
         .expect("Failed to generate code for Sidex bundles.");
 }
