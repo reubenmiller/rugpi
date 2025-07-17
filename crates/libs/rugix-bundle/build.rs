0c1c37888f93c337672c4c7329525b622c4315e1
reubenmiller <reuben.d.miller@gmail.com>
2025-07-17 07:03:26 +0200

    use CARGO_MANIFEST_DIR instead of relative path

--- crates/libs/rugix-bundle/build.rs
+++ crates/libs/rugix-bundle/build.rs
@@ -1,6 +1,7 @@
 fn main() {
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     sidex_build_rs::configure()
-        .with_bundle(".")
+        .with_bundle(path)
         .generate()
         .expect("Failed to generate code for Sidex bundles.");
 }
